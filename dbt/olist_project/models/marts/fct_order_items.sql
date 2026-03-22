09a6dae33b19016002de9f59c4850483fbd31576
Muhammad Rezky Hamesi Akhyar <muhammadrezkyhamesi@gmail.com>
2026-03-22 13:34:29 +0700

    [feature] finish dbt mart

---
+++ dbt/olist_project/models/marts/fct_order_items.sql
@@ -0,0 +1,8 @@
+SELECT
+    order_id,
+    order_item_id,
+    product_id,
+    seller_id,
+    price,
+    freight_value
+FROM staging.stg_order_items
---
+++ dbt/olist_project/models/marts/fct_orders.sql
@@ -0,0 +1,36 @@
+WITH order_items_agg AS (
+
+    SELECT
+        order_id,
+        SUM(price) AS total_item_value,
+        SUM(freight_value) AS total_freight_value
+    FROM staging.stg_order_items
+    GROUP BY order_id
+
+),
+
+orders AS (
+
+    SELECT
+        order_id,
+        customer_id,
+        order_purchase_ts,
+        order_status
+    FROM staging.stg_orders
+
+)
+
+SELECT
+    o.order_id,
+    o.customer_id,
+    o.order_purchase_ts,
+    o.order_status,
+
+    oi.total_item_value,
+    oi.total_freight_value,
+
+    (oi.total_item_value + oi.total_freight_value) AS total_order_value
+
+FROM orders o
+LEFT JOIN order_items_agg oi
+    ON o.order_id = oi.order_id
